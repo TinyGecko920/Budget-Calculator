59556942b8a8364f8a5e8cec28e38bff06de85f7
Tyler Fursman <57113760+TinyGecko920@users.noreply.github.com>
2023-01-31 09:13:28 -0800

    Deduction Data (deductions.js)
    deductions.js contains the data for all deductions. Each deduction has a name, amount, and deduction type (multiply by decimal or subtract a number)

---
+++ scripts/deductions.js
@@ -0,0 +1,21 @@
+/*
+    const deductionsData = require("./deductions.js").data;
+
+    [ Deduction Title, Default Amount, Deduction Type]
+
+    Deduction Types =
+    "%" - Deduct a percentage from gross income, (amount in decimal form)
+    "-" - Subtract a certain amount from gross income
+*/
+
+const data = [
+    [ "Federal Taxes", 0.12, "%" ],
+    [ "State Taxes", 0.07, "%"],
+    [ "Social Security", 0.062, "%" ],
+    [ "Medicare", 0.0145, "%" ],
+    [ "State Disability", 0.01, "%" ],
+    [ "Retirement Investment", 0.05, "%" ]
+    [ "Medical Insurance", 180, "-" ],
+]
+
+exports.data = data
