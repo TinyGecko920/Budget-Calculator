b2074069dc4b84e10563fae04912abb8a664f1da
Tyler Fursman <57113760+TinyGecko920@users.noreply.github.com>
2023-02-01 10:38:16 -0800

    Continued math.js
    Continued adding on to math.js

--- scripts/math.js
+++ scripts/math.js
@@ -1,15 +1,32 @@
-const careerData = require("./career-data.js").data;
-const deductionsData = require("./deductions.js").data;
-
-console.log(careerData);
-console.log(deductionsData);
+const deducitons = [
+    [ "Federal Taxes", 0.12, "%" ],
+    [ "State Taxes", 0.07, "%"],
+    [ "Social Security", 0.062, "%" ],
+    [ "Medicare", 0.0145, "%" ],
+    [ "State Disability", 0.01, "%" ],
+    [ "Retirement Investment", 0.05, "%" ]
+    [ "Medical Insurance", 180, "-" ],
+]
 
 function multiplyNums(a, b) {
     try {
         if(typeof a === "number" && typeof b === "number") {
             return(a * b);
         } else {
-            throw new Error("One or more paramaters passed to function are not numbers.");
+            throw new Error(`One or more paramaters passed to function are not numbers, ${a, b}`);
+        }
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+function divideNums(a, b) {
+    try {
+        if(typeof a === "number" && typeof b === "number") {
+            alert(a / b)
+            return(a / b);
+        } else {
+            throw new Error(`One or more paramaters passed to function are not numbers, ${a, b}`);
         }
     } catch(err) {
         console.log(err);
@@ -21,12 +38,28 @@ function subtractNums(a, b) {
         if(typeof a === "number" && typeof b === "number") {
             return(a - b);
         } else {
-            throw new Error("One or more paramaters passed to function are not numbers.");
+            throw new Error(`One or more paramaters passed to function are not numbers, ${a, b}`);
         }
     } catch(err) {
         console.log(err);
     }
 }
+
+for(let deduction of deducitons) {
+    try {
+        if(deduction[2] === "%" && typeof deduction[1] === "number") {
+            console.log(multiplyNums(10000, deduction[1]))
+        } else if(deduction[2] === "-" && typeof deduction[1] === "number") {
+            console.log(subtractNums(10000, deduction[1]))
+        } else {
+            throw new Error("Test")
+        }
+    } catch(err) {
+        console.log(err)
+    }
+    console.log(deduction[1])
+}
+
 
 console.log(multiplyNums(1000, "c"));
 console.log("still running");
